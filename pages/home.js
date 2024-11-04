1084332d2cade76bb4967d2838112d45fe73cf0a
ChanEilFhios <bobrien@gmail.com>
2024-11-04 17:22:23 +0000

    Setup three panes on home page

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,20 @@
 export default () => {
-    const rootEl = document.createElement("span")
-    rootEl.innerText = "Home Page"
+    const rootEl = document.createElement("div")
+    rootEl.classList.add("row")
+
+    const leftPane = document.createElement("div")
+    leftPane.classList.add("col-4")
+    leftPane.innerText = "Left"
+
+    const middlePane = document.createElement("div")
+    middlePane.classList.add("col-4")
+    middlePane.innerText = "Left"
+
+    const rightPane = document.createElement("div")
+    rightPane.classList.add("col-4")
+    rightPane.innerText = "Left"
+
+    rootEl.replaceChildren([leftPane, middlePane, rightPane])
 
     return rootEl
 }
