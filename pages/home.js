f5af4ea0313704e4ea1d7ed780843f37694b435d
ChanEilFhios <bobrien@gmail.com>
2024-11-04 19:40:34 +0000

    Reformatting

--- pages/home.js
+++ pages/home.js
@@ -4,9 +4,15 @@ export default () => {
     const rootEl = document.createElement("div")
     rootEl.classList.add("row")
 
-    const today = createContainerElem(["col-4"], [createHdr("Today", 1)])
-    const tasks = createContainerElem(["col-4"], [createHdr("Tasks", 1)])
-    const exams = createContainerElem(["col-4"], [createHdr("Exams", 1)])
+    const today = createContainerElem(["col-4"], [
+        createHdr("Today", 1)
+    ])
+    const tasks = createContainerElem(["col-4"], [
+        createHdr("Tasks", 1)
+    ])
+    const exams = createContainerElem(["col-4"], [
+        createHdr("Exams", 1)
+    ])
 
     rootEl.replaceChildren(today, tasks, exams)
 
