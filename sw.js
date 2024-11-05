3994018094146d910db5b17993632ddcc2e1ee17
ChanEilFhios <bobrien@gmail.com>
2024-11-05 18:54:16 +0000

    Add initial service work setup

---
+++ sw.js
@@ -0,0 +1,40 @@
+importScripts(  'https://storage.googleapis.com/workbox.cdn/releases/6.4.1/workbox.sw.js')
+
+const { warmStrategyCache } = workbox.recipes
+const { CacheFirst, staleWhileRevalidated, networkFirst } = workbox.strategies
+const { registerRoute } = workbox.routing
+const { CacheableResponsePlugin } = workbox.cacheable-response
+const { ExpirationPlugin } = workbox.expiration
+
+// Set up page cache
+const pageCache = new networkFirst({
+  cacheName: 'page-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 60, //30 * 24 * 60 * 60,
+    }),
+  ],
+});
+
+warmStrategyCache({
+  urls: ['/index.html', '/'],
+  strategy: pageCache,
+});
+
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+registerRoute(
+    ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+    new networkFirst({
+      cacheName: 'asset-cache',
+      plugins: [
+        new CacheableResponsePlugin({
+          statuses: [0, 200],
+        }),
+      ],
+    }),
+  );
+  
