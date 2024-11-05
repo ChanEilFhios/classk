b4174b6821fe8707a03710e88a8f3787f7575a40
ChanEilFhios <bobrien@gmail.com>
2024-11-05 13:31:57 +0000

    Add borders to home page layout

--- pages/home.js
+++ pages/home.js
@@ -2,7 +2,7 @@ import { row, header, col } from "../scripts/renderutils.js"
 
 export default () => {
     return [
-        row([], [
+        row(['border-bottom'], [
             col(4, [], [
                 header("Today", 1)
             ]),
@@ -14,10 +14,10 @@ export default () => {
             ])
         ]),
         row([], [
-            col(4, [], [
+            col(4, ['border-end'], [
                 header("Today", 1)
             ]),
-            col(4, [], [
+            col(4, ['border-end'], [
                 header("Tasks", 1)
             ]),
             col(4, [], [
