a179820e4769102a496e30e010ac97588d452729
ChanEilFhios <bobrien@gmail.com>
2024-11-05 13:25:00 +0000

    Change home page to return array of elements.

--- pages/home.js
+++ pages/home.js
@@ -1,17 +1,17 @@
 import { row, header, col } from "../scripts/renderutils.js"
 
 export default () => {
-    const rootEl = row([], [
-        col(4, [], [
-            header("Today", 1)
-        ]),
-        col(4, [], [
-            header("Tasks", 1)
-        ]),
-        col(4, [], [
-            header("Exams", 1)
+    return [
+        row([], [
+            col(4, [], [
+                header("Today", 1)
+            ]),
+            col(4, [], [
+                header("Tasks", 1)
+            ]),
+            col(4, [], [
+                header("Exams", 1)
+            ])
         ])
-    ])
-
-    return rootEl
+    ]
 }
