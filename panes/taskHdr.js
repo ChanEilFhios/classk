b2768fb2f5a8973d5883f0252df414b621f30841
ChanEilFhios <bobrien@gmail.com>
2024-11-06 19:28:37 +0000

    Create space between the header and the button

--- panes/taskHdr.js
+++ panes/taskHdr.js
@@ -3,6 +3,6 @@ import { header, button } from "../scripts/renderutils.js"
 export default () => {
     return [
         header('Tasks', 2, ['d-inline', 'align-middle']),
-        button('Add Task', 'primary', ['align-middle'])
+        button('Add Task', 'primary', ['align-middle', 'm-s-1'])
     ]
 }
