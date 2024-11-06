a268d95e5ed136519996ba88148c1af2ea4423a2
ChanEilFhios <bobrien@gmail.com>
2024-11-06 20:18:10 +0000

    Cleaning up...I think i've got it.

--- panes/taskHdr.js
+++ panes/taskHdr.js
@@ -2,7 +2,7 @@ import { header, button } from "../scripts/renderutils.js"
 
 export default () => {
     return [
-        header('Tasks', 2, ['d-inline']),
-        button('+', 'primary', ['btn-sm'])
+        header('Tasks', 2, ['d-inline', 'align-middle']),
+        button('+', 'primary', ['btn-sm', 'align-middle', 'ms-2'])
     ]
 }
