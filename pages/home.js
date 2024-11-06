bf7066a795d96c9c85d5664223a8ae18f7923979
ChanEilFhios <bobrien@gmail.com>
2024-11-06 19:24:12 +0000

    Trying that middle alignment again

--- pages/home.js
+++ pages/home.js
@@ -8,7 +8,7 @@ export default () => {
             col(4, [], [
                 header("Today", 1)
             ]),
-            col(4, ['align-middle'],
+            col(4, [],
                 arrayify(taskHeader())
             ),
             col(4, [], [
--- panes/taskHdr.js
+++ panes/taskHdr.js
@@ -2,7 +2,7 @@ import { header, button } from "../scripts/renderutils.js"
 
 export default () => {
     return [
-        header('Tasks', 2, ["d-inline"]),
-        button('Add Task', "primary")
+        header('Tasks', 2, ['d-inline', 'align-middle']),
+        button('Add Task', 'primary', ['align-middle'])
     ]
 }
