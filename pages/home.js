118b5cf3488fb48d3b6f9356bdd45eb5dd3a4971
ChanEilFhios <bobrien@gmail.com>
2024-11-06 19:20:16 +0000

    Make tasks header smaller and align middle

--- pages/home.js
+++ pages/home.js
@@ -8,7 +8,7 @@ export default () => {
             col(4, [], [
                 header("Today", 1)
             ]),
-            col(4, [],
+            col(4, ['align-middle'],
                 arrayify(taskHeader())
             ),
             col(4, [], [
--- panes/taskHdr.js
+++ panes/taskHdr.js
@@ -2,7 +2,7 @@ import { header, button } from "../scripts/renderutils.js"
 
 export default () => {
     return [
-        header('Tasks', 1, ["d-inline"]),
+        header('Tasks', 2, ["d-inline"]),
         button('Add Task', "primary")
     ]
 }
