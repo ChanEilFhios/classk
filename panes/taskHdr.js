355fd74cf4201b3f41be67112c5e07f9b647eaba
ChanEilFhios <bobrien@gmail.com>
2024-11-06 20:15:33 +0000

    Back to inline?

--- panes/taskHdr.js
+++ panes/taskHdr.js
@@ -2,7 +2,7 @@ import { header, button } from "../scripts/renderutils.js"
 
 export default () => {
     return [
-        header('Tasks', 2, []),
+        header('Tasks', 2, ['d-inline']),
         button('+', 'primary', ['btn-sm'])
     ]
 }
