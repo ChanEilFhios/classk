2660104bdc0234c2e344758e79646eaf6ee4aeca
Brendan O'Brien <bobrien@gmail.com>
2025-01-12 07:46:53 -0500

    Move the addClassModal from today to classes

--- data/classes.js
+++ data/classes.js
@@ -1,8 +1,56 @@
 import van from "vanjs-core"
 import { dataMgr } from "./datamgr.js"
+import formInput, { formInputProperties } from "../components/forminput.js"
+import { Await, Modal } from "vanjs-ui"
+
+const { button, div, form, h1, label, span } = van.tags
 
 export const lastUpdate = van.state(Date.now())
 export const schema = '@id,name,start,end,days'
+
+export const addClassModal = (initialData) => {
+    const closed = van.state(false)
+    const formInputs = [
+        formInputProperties("Name", "classname", "text", {required: true}),
+        formInputProperties("Building", "classbuilding", "text"),
+        formInputProperties("Teacher", "classteacher", "text"),
+        formInputProperties("Room", "classroom", "text"),
+        formInputProperties("Start", "classtart", "time"),
+        formInputProperties("End", "classend", "time")
+    ]
+    
+    const dayInputs = [
+        formInputProperties("Monday", "classmonday", "checkbox"),
+        formInputProperties("Tuesday", "classtuesday", "checkbox"),
+        formInputProperties("Wednesday", "classwednesday", "checkbox"),
+        formInputProperties("Thursday", "classthursday", "checkbox"),
+        formInputProperties("Friday", "classfriday", "checkbox"),
+    ]
+
+    const onOK = (e) => {
+        const formData = new FormData(document.getElementById("classform"))
+
+        addClass(Object.fromEntries(formData))
+        closed.val = true
+    }
+
+    van.add(document.body,
+        Modal({closed}, 
+            form({id: "classform", class: "twocolumn modalform"},
+                ...formInputs.map((properties) => formInput(properties)),
+
+                label("Days"),
+                div({class: "span3"},
+                    ...dayInputs.map((properties) => formInput(properties).reverse())
+                )
+            ),
+            div({class: "modalbuttons"},
+                button({onclick: onOK}, "Ok"),
+                button({onclick: () => closed.val = true}, "Cancel")
+            )
+        )
+    )
+}
 
 export const getClasses = () => {
     return dataMgr().class.toArray()
--- panes/today.js
+++ panes/today.js
@@ -3,7 +3,7 @@ import columnhdr from "../components/columnhdr.js"
 import formInput, { formInputProperties } from "../components/forminput.js"
 import { Await, Modal } from "vanjs-ui"
 import pane from "../components/pane.js"
-import { getClasses, addClass, lastUpdate } from "../data/classes.js"
+import { getClasses, addClassModal, lastUpdate } from "../data/classes.js"
 
 const { button, div, form, h1, label, span } = van.tags
 
@@ -44,48 +44,4 @@ export const classesPane = (position, properties = {}) => {
                             }
                         }
                     )
-}
-
-export const addClassModal = (initialData) => {
-    const closed = van.state(false)
-    const formInputs = [
-        formInputProperties("Name", "classname", "text", {required: true}),
-        formInputProperties("Building", "classbuilding", "text"),
-        formInputProperties("Teacher", "classteacher", "text"),
-        formInputProperties("Room", "classroom", "text"),
-        formInputProperties("Start", "classtart", "time"),
-        formInputProperties("End", "classend", "time")
-    ]
-    
-    const dayInputs = [
-        formInputProperties("Monday", "classmonday", "checkbox"),
-        formInputProperties("Tuesday", "classtuesday", "checkbox"),
-        formInputProperties("Wednesday", "classwednesday", "checkbox"),
-        formInputProperties("Thursday", "classthursday", "checkbox"),
-        formInputProperties("Friday", "classfriday", "checkbox"),
-    ]
-
-    const onOK = (e) => {
-        const formData = new FormData(document.getElementById("classform"))
-
-        addClass(Object.fromEntries(formData))
-        closed.val = true
-    }
-
-    van.add(document.body,
-        Modal({closed}, 
-            form({id: "classform", class: "twocolumn modalform"},
-                ...formInputs.map((properties) => formInput(properties)),
-
-                label("Days"),
-                div({class: "span3"},
-                    ...dayInputs.map((properties) => formInput(properties).reverse())
-                )
-            ),
-            div({class: "modalbuttons"},
-                button({onclick: onOK}, "Ok"),
-                button({onclick: () => closed.val = true}, "Cancel")
-            )
-        )
-    )
 }
