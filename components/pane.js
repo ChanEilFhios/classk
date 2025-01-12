28ed3563b39cb32aa67eeeb7cc56ac1dd340e831
Brendan O'Brien <bobrien@gmail.com>
2025-01-12 08:03:09 -0500

    Separate today pane from classes pane

--- components/pane.js
+++ components/pane.js
@@ -1,10 +1,23 @@
 import van from "vanjs-core"
 import { handleElementParams } from "../utilities/display.js"
 
-const { div } = van.tags
+const { div, h1 } = van.tags
 
 export default (position = "", propsOrChild = {}, ...childList) => {
-    const {properties, children} = handleElementParams(propsOrChild, childList)
+  const { properties, children } = handleElementParams(propsOrChild, childList)
 
-    return div({...properties, class: `${properties.class || ""} pane ${position}`}, children)
+  return div(
+    { ...properties, class: `${properties.class || ""} pane ${position}` },
+    children
+  )
+}
+
+export const paneSection = (heading, propsOrChild = {}, ...childList) => {
+  const { properties, children } = handleElementParams(propsOrChild, childList)
+  children.unshift(h1({ class: "panesectionheading" }, heading))
+
+  return div(
+    { ...properties, class: `${properties.class || ""} panesection` },
+    children
+  )
 }
--- panes/today.js
+++ panes/today.js
@@ -1,47 +1,44 @@
 import van from "vanjs-core"
 import columnhdr from "../components/columnhdr.js"
-import formInput, { formInputProperties } from "../components/forminput.js"
-import { Await, Modal } from "vanjs-ui"
-import pane from "../components/pane.js"
+import { Await } from "vanjs-ui"
+import pane, { paneSection } from "../components/pane.js"
 import { getClasses, addClassModal, lastUpdate } from "../data/classes.js"
 
-const { button, div, form, h1, label, span } = van.tags
+const { button, h1, span } = van.tags
 
 export const todayHdr = (position, properties = {}) =>
-    columnhdr(position, properties,
-        span(
-            h1("Today"),
-            span(new Date().toDateString()),
-        ),
-        button({onclick: () => addClassModal()}, "Add Class..."),
-    )
+  columnhdr(
+    position,
+    properties,
+    span(h1("Today"), span(new Date().toDateString())),
+    button({ onclick: () => addClassModal() }, "Add Class...")
+  )
 
 export const todayPane = (position, properties = {}) =>
-    pane(position, properties,
-        div("Classes"),
-        div("Tomorrow")
-    )
+  pane(position, properties, paneSection("Classes", properties, classesPane()))
 
 export const classesPane = (position, properties = {}) => {
-    let previousUpdate = 0
+  let previousUpdate = 0
 
-    return () => pane(position, properties, h1("Classes:"),
-                        (dom) => {
-                            if (lastUpdate.val !== previousUpdate) {
-                                previousUpdate = lastUpdate.val
-                                const classes = van.state(getClasses())
+  return () =>
+    pane(position, properties, (dom) => {
+      if (lastUpdate.val !== previousUpdate) {
+        previousUpdate = lastUpdate.val
+        const classes = van.state(getClasses())
 
-                                return Await({
-                                    value: classes.val,
-                                    Loading: () => "🌀 Loading...",
-                                    Error: (e) => `Unable to load: ${e}`
-                                }, (classList) =>{
-                                    console.log("rendering", classList)
-                                    return classList
-                                })
-                            } else {
-                                return dom
-                            }
-                        }
-                    )
+        return Await(
+          {
+            value: classes.val,
+            Loading: () => "🌀 Loading...",
+            Error: (e) => `Unable to load: ${e}`,
+          },
+          (classList) => {
+            console.log("rendering", classList)
+            return classList
+          }
+        )
+      } else {
+        return dom
+      }
+    })
 }
