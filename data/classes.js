7d36e2e0f59a7a0f624317c260e7f207f01e272d
Brendan O'Brien <bobrien@gmail.com>
2025-01-12 08:31:15 -0500

    Move classesList to classes

--- data/classes.js
+++ data/classes.js
@@ -3,82 +3,113 @@ import { dataMgr } from "./datamgr.js"
 import formInput, { formInputProperties } from "../components/forminput.js"
 import { Await, Modal } from "vanjs-ui"
 
-const { button, div, form, h1, label, span } = van.tags
+const { button, div, form, label } = van.tags
 
 export const lastUpdate = van.state(Date.now())
-export const schema = '@id,name,start,end,days'
+export const schema = "@id,name,start,end,days"
+
+export const classesList = () => {
+  let previousUpdate = 0
+
+  return () =>
+    div((dom) => {
+      if (lastUpdate.val !== previousUpdate) {
+        previousUpdate = lastUpdate.val
+        const classes = van.state(getClasses())
+
+        return Await(
+          {
+            value: classes.val,
+            Loading: () => "🌀 Loading...",
+            Error: (e) => `Unable to load: ${e}`,
+          },
+          (classList) => {
+            console.log("rendering", classList)
+            return classList
+          }
+        )
+      } else {
+        return dom
+      }
+    })
+}
 
 export const addClassModal = (initialData) => {
-    const closed = van.state(false)
-    const formInputs = [
-        formInputProperties("Name", "classname", "text", {required: true}),
-        formInputProperties("Building", "classbuilding", "text"),
-        formInputProperties("Teacher", "classteacher", "text"),
-        formInputProperties("Room", "classroom", "text"),
-        formInputProperties("Start", "classtart", "time"),
-        formInputProperties("End", "classend", "time")
-    ]
-    
-    const dayInputs = [
-        formInputProperties("Monday", "classmonday", "checkbox"),
-        formInputProperties("Tuesday", "classtuesday", "checkbox"),
-        formInputProperties("Wednesday", "classwednesday", "checkbox"),
-        formInputProperties("Thursday", "classthursday", "checkbox"),
-        formInputProperties("Friday", "classfriday", "checkbox"),
-    ]
+  const closed = van.state(false)
+  const formInputs = [
+    formInputProperties("Name", "classname", "text", { required: true }),
+    formInputProperties("Building", "classbuilding", "text"),
+    formInputProperties("Teacher", "classteacher", "text"),
+    formInputProperties("Room", "classroom", "text"),
+    formInputProperties("Start", "classtart", "time"),
+    formInputProperties("End", "classend", "time"),
+  ]
 
-    const onOK = (e) => {
-        const formData = new FormData(document.getElementById("classform"))
+  const dayInputs = [
+    formInputProperties("Monday", "classmonday", "checkbox"),
+    formInputProperties("Tuesday", "classtuesday", "checkbox"),
+    formInputProperties("Wednesday", "classwednesday", "checkbox"),
+    formInputProperties("Thursday", "classthursday", "checkbox"),
+    formInputProperties("Friday", "classfriday", "checkbox"),
+  ]
 
-        addClass(Object.fromEntries(formData))
-        closed.val = true
-    }
+  const onOK = (e) => {
+    const formData = new FormData(document.getElementById("classform"))
 
-    van.add(document.body,
-        Modal({closed}, 
-            form({id: "classform", class: "twocolumn modalform"},
-                ...formInputs.map((properties) => formInput(properties)),
+    addClass(Object.fromEntries(formData))
+    closed.val = true
+  }
 
-                label("Days"),
-                div({class: "span3"},
-                    ...dayInputs.map((properties) => formInput(properties).reverse())
-                )
-            ),
-            div({class: "modalbuttons"},
-                button({onclick: onOK}, "Ok"),
-                button({onclick: () => closed.val = true}, "Cancel")
-            )
+  van.add(
+    document.body,
+    Modal(
+      { closed },
+      form(
+        { id: "classform", class: "twocolumn modalform" },
+        ...formInputs.map((properties) => formInput(properties)),
+
+        label("Days"),
+        div(
+          { class: "span3" },
+          ...dayInputs.map((properties) => formInput(properties).reverse())
         )
+      ),
+      div(
+        { class: "modalbuttons" },
+        button({ onclick: onOK }, "Ok"),
+        button({ onclick: () => (closed.val = true) }, "Cancel")
+      )
     )
+  )
 }
 
 export const getClasses = () => {
-    return dataMgr().class.toArray()
+  return dataMgr()
+    .class.toArray()
     .then((allClasses) => {
-        return allClasses.map((aClass) => aClass.name).join("<br/>")
+      return allClasses.map((aClass) => aClass.name).join("<br/>")
     })
     .then((classlist) => {
-        return classlist
+      return classlist
     })
 }
 
 export const addClass = (newClass) => {
-    dataMgr().class.put(
-        {
-            name: newClass.Name,
-            start: newClass.Start,
-            end: newClass.End,
-            building: newClass.Building,
-            room: newClass.Room,
-            teacher: newClass.Teacher,
-            days: {
-                monday: newClass.Monday,
-                tuesday: newClass.Tuesday,
-                wednesday: newClass.Wednesday,
-                thursday: newClass.Thursday,
-                friday: newClass.friday
-            }
-
-        }
-    ).then(() => lastUpdate.val = Date.now())
+  dataMgr()
+    .class.put({
+      name: newClass.Name,
+      start: newClass.Start,
+      end: newClass.End,
+      building: newClass.Building,
+      room: newClass.Room,
+      teacher: newClass.Teacher,
+      days: {
+        monday: newClass.Monday,
+        tuesday: newClass.Tuesday,
+        wednesday: newClass.Wednesday,
+        thursday: newClass.Thursday,
+        friday: newClass.friday,
+      },
+    })
+    .then(() => (lastUpdate.val = Date.now()))
 }
--- panes/today.js
+++ panes/today.js
@@ -2,7 +2,7 @@ import van from "vanjs-core"
 import columnhdr from "../components/columnhdr.js"
 import { Await } from "vanjs-ui"
 import pane, { paneSection } from "../components/pane.js"
-import { getClasses, addClassModal, lastUpdate } from "../data/classes.js"
+import { classesList, addClassModal } from "../data/classes.js"
 
 const { button, div, h1, span } = van.tags
 
@@ -16,29 +16,3 @@ export const todayHdr = (position, properties = {}) =>
 
 export const todayPane = (position, properties = {}) =>
   pane(position, properties, paneSection("Classes", classesList()))
-
-export const classesList = () => {
-  let previousUpdate = 0
-
-  return () =>
-    div((dom) => {
-      if (lastUpdate.val !== previousUpdate) {
-        previousUpdate = lastUpdate.val
-        const classes = van.state(getClasses())
-
-        return Await(
-          {
-            value: classes.val,
-            Loading: () => "🌀 Loading...",
-            Error: (e) => `Unable to load: ${e}`,
-          },
-          (classList) => {
-            console.log("rendering", classList)
-            return classList
-          }
-        )
-      } else {
-        return dom
-      }
-    })
-}
